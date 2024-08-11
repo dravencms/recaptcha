e718a45e997a8b53357b54e4bc2b3f756164cf7c
Adam Schubert <adam.schubert@sg1-game.net>
2024-08-11 15:07:13 +0200

    fix: Add missing use

--- src/Forms/ReCaptchaField.php
+++ src/Forms/ReCaptchaField.php
@@ -3,6 +3,7 @@
 namespace Dravencms\ReCaptcha\Forms;
 
 use Contributte\ReCaptcha\Forms\ReCaptchaField as FormsReCaptchaField;
+use Dravencms\Captcha\Forms\ICaptchaField;
 
 
 class ReCaptchaField extends FormsReCaptchaField implements ICaptchaField
