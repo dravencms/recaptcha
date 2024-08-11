eec27f2ff41f3394558469d482cecc78e783d784
Adam Schubert <adam.schubert@sg1-game.net>
2024-08-11 15:07:48 +0200

    fix: Incorrect use

--- src/ReCaptchaProvider.php
+++ src/ReCaptchaProvider.php
@@ -5,7 +5,7 @@ namespace Dravencms\ReCaptcha;
 use Contributte\ReCaptcha\ReCaptchaProvider as ReCaptchaReCaptchaProvider;
 use Dravencms\Captcha\ICaptchaProvider;
 use Dravencms\Captcha\Forms\ICaptchaField;
-use Dravencms\Captcha\Forms\ReCaptchaField;
+use Dravencms\ReCaptcha\Forms\ReCaptchaField;
 
 class ReCaptchaProvider extends ReCaptchaReCaptchaProvider implements ICaptchaProvider
 {
